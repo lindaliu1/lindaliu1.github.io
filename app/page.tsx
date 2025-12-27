7b4bea1289047a7fbb7bf83f1ea2a3e1ef493c0f
Linda Liu <lindaliu444@gmail.com>
2025-12-26 17:54:10 -0800

    ya

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,10 @@ export default function Home() {
         <li>interested in data analysis/science, relational databases, and software development</li>
         <li>learning pottery at vancouver ceramics studio</li>
       </ul>
-      <p>i'd love to talk! feel free to reach out through <a className="social-media-links" href="mailto:lindaliu444@gmail.com">email</a> or <a className="social-media-links" href="https://www.linkedin.com/in/linda-liu1/">linkedin</a>. 
+      <p style={{ marginBottom: "5rem" }}>i'd love to talk! feel free to reach out through <a className="social-media-links" href="mailto:lindaliu444@gmail.com">email</a> or <a className="social-media-links" href="https://www.linkedin.com/in/linda-liu1/">linkedin</a>.
       or, you can find me on <a className="social-media-links" href="https://github.com/lindaliu1">github</a>, <a className="social-media-links" href="https://open.spotify.com/user/lindamaybe?si=95f68b1e225d4316">spotify</a>, and <a className="social-media-links" href="https://beliapp.co/app/lindaliu">beli</a>!</p>
+
+      <p>last updated: december 2025</p>
     </>
   );
 }
